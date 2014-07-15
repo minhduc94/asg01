01bc95650ea008615699f53a3bc57dd0ee97e044
minhduc94 <minhduc.hn94@gmail.com>
2014-07-15 22:46:27 +0700

    first commit

---
+++ asg01.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class asg01 {
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Ho va ten: Pham Minh Duc");
+		System.out.println("Ma sinh vien: 12020105");
+		System.out.println("Lop: K57CC");
+		System.out.println("Tai khoan gitHub: minhduc94");
+		System.out.println("Dia chi email: minhduc.hn94@gmail.com");
+		for (int i = 20; i >0; i--){
+			System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+			System.out.println("Take one down, pass it around, ");
+		}
+		System.out.println("No more bottles of beer on the wall");
+	}
+}
